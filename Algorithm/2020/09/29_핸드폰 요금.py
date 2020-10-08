e5e874d6d0e7ed7b48ef4b0a41e629c2b616911c
Gim <14997@global.ul.com>
2020-10-08 16:44:00 +0900

    10/08_알고리즘기초

--- Algorithm/2020/09/29_핸드폰 요금.py
+++ Algorithm/2020/09/29_핸드폰 요금.py
@@ -1,7 +1,28 @@
-# 30초 마다 10원
-# 60초 마다 15월
+# Y : 30초 마다 10원
+# M : 60초 마다 15월
 # 20보다 큰 자연수의 통화시간이 주어진다. 더 싼 요금제와 금액을 써라
 
 call = int(input())
-c_times = map(int, input().split())
+c_times = list(map(int, input().split()))
 
+Y_sum = []
+M_sum = []
+
+for i in range(call):
+    if c_times[i] % 30 == 0:
+        Y_sum.append(10 * (c_times[i] // 30))
+    else:
+        Y_sum.append(10*(c_times[i] // 30) + 10)
+
+for i in range(call):
+    if c_times[i] % 60 == 0:
+        M_sum.append(15 * (c_times[i] // 60))
+    else:
+        M_sum.append(15*(c_times[i] // 60) + 15)
+
+if sum(Y_sum) < sum(M_sum):
+    print('Y', sum(Y_sum))
+elif sum(Y_sum) > sum(M_sum):
+    print('M', sum(M_sum))
+else:
+    print('Y M', sum(Y_sum))
--- Algorithm/2020/09/Practice.py
+++ Algorithm/2020/09/Practice.py
@@ -7,11 +7,15 @@
 # print(8, end=', ')
 # print(9)
 
-a = [1,1,2,2,3,3]
-b = list(set(a))
-c = [1,1, 4 ,5]
-k = c.count(1)
+# a = [1,1,2,2,3,3]
+# b = list(set(a))
+# c = [1,1, 4 ,5]
+# k = c.count(1)
+#
+# print(b)
+# print(list(set(a).intersection(c)))
+# print(k)
 
-print(b)
-print(list(set(a).intersection(c)))
-print(k)
+print(32//10)
+print(32%10)
+print(32/10)
